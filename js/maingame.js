b24f73b1bb23e3a90d41d608dc3a71c482786f42
jasonaul <jason.aul@gmail.com>
2022-06-21 00:47:50 -0400

    Started the buy options with alerts

--- js/maingame.js
+++ js/maingame.js
@@ -3,8 +3,29 @@ console.log("Oh well.")
 
 const player = {
     earnings: 0,
+    tools: [
+        "teeth", 
+    ]
 };
 
+let tools = [
+{   type: "Teeth",
+    pay: 1,
+},
+{   type: "Scissors",
+    pay: 5,
+},
+{   type: "Lawnmower",
+    pay: 50,
+},
+{   type: "Gas Lawnmower",
+    pay: 100,
+},
+{
+    type: "Students",
+    pay: 250,
+}
+];
 
 
 
@@ -31,11 +52,26 @@ function mowTeeth() {
         }
 
 function mowScissors() {
+    if (player.tools.includes("Scissors")){
     player.earnings ===  (player.earnings+=5)
         document.getElementById('earnings').innerHTML = "You have earned $" + player.earnings + ".";
-    
+    } else {
+        alert("You need to buy the scissors first!")
+    }
             }
          
+////Buying Things
+
+function buyScissors() {
+    if (!player.tools.includes("Scissors")) {
+     player.earnings === (player.earnings-=5)
+    player.tools.push("Scissors")
+    document.getElementById('earnings').innerHTML = "You have earned $" + player.earnings + ".";
+    } else { (player.tools.includes("Scissors")) 
+        alert("You already own the scissors!")
+    }
+}
+
 
 
 console.log(player.earnings)
